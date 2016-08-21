c03458bfd2d645bf2ad2d8be2b2742d9dbc44537
Von Galt <eslcomputertraining@gmail.com>
2016-08-20 18:24:37 -0700

    Update pins_controller.rb

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -8,4 +8,25 @@ class PinsController < ApplicationController
     @pin = Pin.find(params[:id])
   end
   
+  def show_by_name
+    @pin = Pin.find_by_slug(params[:slug])
+    @pins = Pin.all
+    render :show
+  end
+
+  RSpec.describe PinsController do
+    describe "GET index" do
+ 
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template("index")
+    end
+  
+    it 'populates @pins with all pins' do
+      get :index
+      expect(assigns[:pins]).to eq(Pin.all)
+
+    end
+    
+  end
 end

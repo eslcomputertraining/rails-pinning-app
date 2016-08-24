ecd6b1e7de94e6910b2e4570637d218d9b2bef71
Von Galt <eslcomputertraining@gmail.com>
2016-08-23 19:18:11 -0700

    committing new changes

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -14,19 +14,4 @@ class PinsController < ApplicationController
     render :show
   end
 
-  RSpec.describe PinsController do
-    describe "GET index" do
- 
-    it 'renders the index template' do
-      get :index
-      expect(response).to render_template("index")
-    end
-  
-    it 'populates @pins with all pins' do
-      get :index
-      expect(assigns[:pins]).to eq(Pin.all)
-
-    end
-    
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,25 @@
 Rails.application.routes.draw do
+  resources :users, except: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'pins#index'
-
+    get 'pins/name-:slug' => 'pins#show_by_name'
+    
+    #get 'signup' => "users#new", as: :signup
+    
+    #get 'login' => "users#login", as: :login
+    
+    #post '/login' => "users#authenticate"
+    
+   # delete '/logout/:id' => "users#logout", as: :logout
+    
   resources :pins
     
+  get '/library' => 'pins#index'
+
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
